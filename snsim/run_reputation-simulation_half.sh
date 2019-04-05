f9318fbb83140e77cdcabe9a3a38c47e6aa6a236
Deborah Duong <deborahvakasduong@gmail.com>
2019-04-05 06:57:12 -0700

    Update run_reputation-simulation_half.sh

--- snsim/run_reputation-simulation_half.sh
+++ snsim/run_reputation-simulation_half.sh
@@ -1,7 +1,8 @@
-cd snsim/reputation
-python3 ~/reputation-simulation/snsim/reputation_simulation/ReputationSim.py s2NoRepHalf_debug100.json
+#begin in reputation-simulation
+cd snsim
+python3 reputation_simulation/ReputationSim.py s2NoRepHalf_debug100.json
 cp s2NoRepHalf_debug100.json test.json
-python -m unittest discover -s /home/reputation/snsim/reputation/test -t /home/reputation/snsim/reputation/test -p *Tests*
-python3 ~/reputation-simulation/snsim/reputation_simulation/ReputationSim.py s2RepHalf_debug100.json
+python -m unittest discover -s test -t test -p *Tests*
+python3 reputation_simulation/ReputationSim.py s2RepHalf_debug100.json
 cp s2RepHalf_debug100.json test.json
-python -m unittest discover -s /home/reputation/snsim/reputation/test -t /home/reputation/snsim/reputation/test -p *Tests*
+python -m unittest discover -s test -t test -p *Tests*
