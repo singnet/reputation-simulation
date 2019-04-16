427c8e402f68a297bd494c95506343959f4f2611
Deborah Duong <deborahvakasduong@gmail.com>
2019-04-16 03:26:26 -0700

    Update run_reputation-simulation_half_reproduce.sh

--- snsim/run_reputation-simulation_half_reproduce.sh
+++ snsim/run_reputation-simulation_half_reproduce.sh
@@ -2,5 +2,5 @@
 #before running do a chmod +x run_reputation-simulation_half_reproduce.sh
 #to remove any windows /r, run  sed -i 's/\r//g' run_reputation-simulation_half_reproduce.sh
 cd snsim
-python3 reputation_simulation/ReputationSim.py s2RepHalf_debug100.json
-python3 reputation_simulation/ReputationSim.py weightDenomUnratedConserv9Ratings5Spending5half_debug100.json
+python3 reputation_simulation/ReputationSim.py noIndividuality1_Weighted.json
+python3 reputation_simulation/ReputationSim.py noIndividuality1_TOM.json
