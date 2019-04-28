b37fe131937b8eff95db501e54c7556fdca28316
Deborah Duong <deborahvakasduong@gmail.com>
2019-04-27 19:50:59 -0700

    Delete run_reputation-simulation_half.sh

--- snsim/run_reputation-simulation_half.sh
+++
@@ -1,8 +0,0 @@
-#begin in reputation-simulation
-cd snsim
-python3 reputation_simulation/ReputationSim.py s2NoRepHalf_debug100.json
-cp s2NoRepHalf_debug100.json test.json
-python -m unittest discover -s test -t test -p *Tests*
-python3 reputation_simulation/ReputationSim.py s2RepHalf_debug100.json
-cp s2RepHalf_debug100.json test.json
-python -m unittest discover -s test -t test -p *Tests*
